ff1634e50c7d4a23d19dd7a9657743125be7bc9d
Fizenshi.dev <106608110+Fizenshidev@users.noreply.github.com>
2025-04-13 04:41:19 +0700

    Update App.tsx

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import TodoList from '../src/components/todolist';
 
 const App = () => {
   return (
     <div>
       <h1>My To-Do List</h1>
+      <TodoList />
     </div>
   );
 };
